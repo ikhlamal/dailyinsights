d35800e417e8311f92c52cff3eb1b9e485530780
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-05-25 19:09:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ def load_image(url):
 
 # URL gambar dan teks summary
 image_urls = [
-    "https://assets-global.website-files.com/659c117df71d5be8c2a540f6/6651b7a78f7dc11bf61f69c1_aleix-espargaro_169.jpeg",
-    "https://example.com/image2.jpg",
-    "https://example.com/image3.jpg",
-    "https://example.com/image4.jpg"
+    "https://akcdn.detik.net.id/visual/2019/02/08/08e695e3-e6fc-4de8-b200-38bbf1a4d618_169.jpeg?w=650&q=90",
+    "https://asset.kompas.com/crops/9yieMQZ2-cwuQWaPTXrs5QqS9BA=/0x0:998x665/750x500/data/photo/2024/02/04/65bf65a465848.jpg",
+    "https://akcdn.detik.net.id/visual/2024/02/08/kader-pdi-p-basuki-tjahja-purnama-alias-ahok-1_169.jpeg?w=650&q=90",
+    "https://cdnv.detik.com/videoservice/AdminTV/2024/05/25/59a36e70287f4204b14a6ce9667dd872-20240525130450-0s.jpg?w=650&q=80"
 ]
 
 summaries = [
