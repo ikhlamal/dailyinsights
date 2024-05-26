abd0d25344daf6c24a59efc6944342a6638388d1
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-05-26 14:34:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def resize_image(img, target_ratio=(3, 2)):
 
 # URL gambar dan teks summary
 title = [
-    "At least 10 people killed in drone attack on Gaza school used as shelter",
+    "Setidaknya 10 Orang Tewas dalam Serangan Drone di Sekolah Gaza yang Digunakan sebagai Tempat Perlindungan",
     "PDI-P Sebut Ahok Siap Maju Pilgub Sumut, Jadi Penantang Bobby",
     "Masuk Bursa Cagub Sumut, Ahok Tunggu Penugasan PDIP",
     "Ahok Tunggu Tugas dari Partai Usai Disebut DPD PDIP Siap Maju Pilgub Sumut"
