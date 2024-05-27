1ba50ca8def0f629bf39038a4725dc9c7499428d
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-05-27 08:02:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ with st.container(border=True):
     # Menambahkan segmen untuk masing-masing bias politik pada bar
     for dataset in df['Dataset'].unique():
         for i, row in df[df['Dataset'] == dataset].iterrows():
-            if dataset == 'Dataset 1':
+            if dataset == 'Kompas':
                 fig.add_trace(go.Bar(
                     x=[row['Percentage']], y=[dataset],
                     name=row['Bias'],
