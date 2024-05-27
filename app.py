e2bb92b1b466fb501a96fdd46f41cce01063084c
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-05-27 18:52:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -341,8 +341,7 @@ with st.container(border=True):
                         name=row['Bias'],
                         orientation='h',
                         marker=dict(color='blue' if row['Bias'] == 'Left' else 'green' if row['Bias'] == 'Center' else 'red'),
-                        hoverinfo='x',
-                        st.write("Detik cenderung menghadirkan berita secara netral tanpa bias politik, sehingga menampilkan berita tersebut dengan porsi yang seimbang berdasarkan fakta yang ada.")
+                        hoverinfo='x'
                     ))
                 else:
                     fig.add_trace(go.Bar(
